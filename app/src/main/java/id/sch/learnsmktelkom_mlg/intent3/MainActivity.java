879c3d1e62b5d60b8f3da94d352d587fa465be84
SavinaRP <savputramadhani@gmail.com>
2016-10-09 21:01:37 +0700

    Intent Telepon

---
+++ app/src/main/java/id/sch/learnsmktelkom_mlg/intent3/MainActivity.java
@@ -0,0 +1,29 @@
+package id.sch.learnsmktelkom_mlg.intent3;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageViewPhone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber("0341712500");
+            }
+        });
+    }
+
+    private void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+    }
+}
